eaa45a8638d18ab28028ee8faa2b1fb68204d1fe
Michael Liu <michael.liu95@gmail.com>
2014-05-31 19:08:07 -0400

    slider edits.

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ $(window).load(function() {
 
 $(document).ready(function(){
   setInterval(function(){
-    if($(".selected").offset().top > ($(window).height()/2) + 10){
+    if($(".selected").offset().top + $(".selected").height()/2 > ($(window).height()/2) + 10){
       $("#offset").css("margin-top", function(index, curValue){
-        return parseInt(curValue, 10) - ($(".selected").offset().top - $(window).height()/2)*3/4 + 'px';
+        return parseInt(curValue, 10) - ($(".selected").offset().top + $(".selected").height()/2 - $(window).height()/2)*3/4 + 'px';
       });
-    } else if($(".selected").offset().top < ($(window).height()/2) - 10){
+    } else if($(".selected").offset().top + $(".selected").height()/2 < ($(window).height()/2) - 10){
       $("#offset").css("margin-top", function(index, curValue){
-        return parseInt(curValue, 10) + ($(window).height()/2 - $(".selected").offset().top)*3/4 + 'px';
+        return parseInt(curValue, 10) + ($(window).height()/2 - $(".selected").height()/2 - $(".selected").offset().top)*3/4 + 'px';
       });
     }
   }, 100);
