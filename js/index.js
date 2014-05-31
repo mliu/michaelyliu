8f6d905178d5d5aa527a1076a7cba384dc7a9506
Michael Liu <michael.liu95@gmail.com>
2014-05-31 18:59:36 -0400

    slider done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-$("window").load(function() {
+$(window).load(function() {
   $("body").removeClass("preload");
+});
+
+$(document).ready(function(){
+  setInterval(function(){
+    if($(".selected").offset().top > ($(window).height()/2) + 10){
+      $("#offset").css("margin-top", function(index, curValue){
+        return parseInt(curValue, 10) - ($(".selected").offset().top - $(window).height()/2)*3/4 + 'px';
+      });
+    } else if($(".selected").offset().top < ($(window).height()/2) - 10){
+      $("#offset").css("margin-top", function(index, curValue){
+        return parseInt(curValue, 10) + ($(window).height()/2 - $(".selected").offset().top)*3/4 + 'px';
+      });
+    }
+  }, 100);
+
+  $(".about-list-item").click(function(){
+    $(".selected").removeClass("selected");
+    $(this).addClass("selected");
+  });
 });
