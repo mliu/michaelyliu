0a69b18b026b3aa01dc1146f7a170b6dd488ab88
Michael Liu <michael.liu95@gmail.com>
2014-06-01 07:49:55 -0400

    content

--- js/index.js
+++ js/index.js
@@ -16,7 +16,11 @@ $(document).ready(function(){
   }, 100);
 
   $(".about-list-item").click(function(){
+    var slide = $(".selected").data("modal-bind");
     $(".selected").removeClass("selected");
+    $("body").find("[data-modal='" + slide + "']").removeClass("visible");
     $(this).addClass("selected");
+    slide = $(this).data("modal-bind");
+    $("body").find("[data-modal='" + slide + "']").addClass("visible");
   });
 });
